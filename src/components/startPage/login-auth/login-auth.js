a02ad47d422a2b12607de385c49b265c32082acf
a-sapon <sapon.anya@gmail.com>
2020-01-21 16:50:28 +0200

    added login functionality

--- src/components/startPage/login-auth/login-auth.js
+++ src/components/startPage/login-auth/login-auth.js
@@ -1,35 +1,95 @@
 import services from '../../../services';
-console.log(services)
+import '../../../../node_modules/pnotify/dist/PNotifyBrightTheme.css';
+import PNotify from '../../../../node_modules/pnotify/dist/es/PNotify.js';
 
 const refs = {
+  overlay: document.querySelector('.auth-modal-overlay'),
   authModalWindow: document.querySelector('.auth-modal'),
   headerRegistrationBtn: document.querySelector('.registration-btn'),
-  registerNewUserBtn: document.querySelector('.btn-registration'),
-  nameInput: document.querySelector('input[name="name"]')
-}
+  inputsContainer: document.querySelector('.inputs-container'),
+  authBtns: document.querySelector('.auth-btns'),
+  logout: document.querySelector('.logout')
+};
 
 refs.headerRegistrationBtn.addEventListener('click', openModal);
-refs.authModalWindow.addEventListener('click', closeModal);
-refs.registerNewUserBtn.addEventListener('click', openRegisterModal);
+refs.overlay.addEventListener('click', closeModal);
+refs.authModalWindow.addEventListener('click', registerOrLogin);
+refs.logout.addEventListener('click', logout);
 
 function openModal() {
-  refs.authModalWindow.classList.remove('hide');
+  refs.overlay.classList.remove('hide');
 }
 
 function closeModal(e) {
-  // + close whenclicked outside modal
-  console.log(e.target);
-  if(e.target.classList.contains('close-icon')) {
-    refs.authModalWindow.classList.add('hide');
+  if (
+    e.target.classList.contains('close-icon') ||
+    e.target === e.currentTarget
+  ) {
+    refs.overlay.classList.add('hide');
   }
 }
 
-function openRegisterModal(e) {
-  refs.nameInput.classList.remove('hide');
-  const user = {
-    name: e.currentTarget.elements.name.value,
-    email: e.currentTarget.elements.email.value,
-    password: e.currentTarget.elements.password.value
+const user = {};
+
+async function registerOrLogin(e) {
+  e.preventDefault();
+  if (e.target.classList.contains('btn-registration')) {
+    const emailInput = e.currentTarget.elements.email;
+    const passwordInput = e.currentTarget.elements.password;
+    const nameInput = document.createElement('input');
+    nameInput.type = 'text';
+    nameInput.placeholder = 'Имя';
+    refs.inputsContainer.insertBefore(nameInput, emailInput);
+    user.name = nameInput.value;
+    user.email = emailInput.value;
+    user.password = passwordInput.value;
+    console.log('user :', user);
+    refs.authBtns.innerHTML = '';
+    const confirmBtn = document.createElement('button');
+    confirmBtn.type = 'submit';
+    confirmBtn.classList.add('btn-confirm');
+    confirmBtn.textContent = 'Подтвердить';
+    refs.authBtns.append(confirmBtn);
+
+    confirmBtn.addEventListener('click', submitRegistrationInfo);
+
+    async function submitRegistrationInfo() {
+      user.name = nameInput.value;
+      user.email = emailInput.value;
+      user.password = passwordInput.value;
+      const dataRegister = await services
+        .postRegisterNewUser(user)
+        .then(console.log)
+        .catch(e => console.log(e));
+      localStorage.setItem('token', dataRegister.data.token);
+    }
+  } else if (e.target.classList.contains('btn-login')) {
+    try {
+      const user = {
+        email: e.currentTarget.elements.email.value,
+        password: e.currentTarget.elements.password.value
+      };
+      const dataLogin = await services.postLoginUser(user);
+      refs.overlay.classList.add('hide');
+      PNotify.success({
+        title: 'Success!',
+        text: 'You are logged in.'
+      });
+      localStorage.setItem('token', dataLogin.data.token);
+    } catch (e) {
+      PNotify.error({
+        title: 'Error',
+        text: 'Email or password is incorrect.',
+        modules: {
+          Desktop: {
+            desktop: true
+          }
+        }
+      });
+    }
   }
-  services.postRegisterNewUser(user);
+}
+
+function logout() {
+  localStorage.removeItem('token');
 }
