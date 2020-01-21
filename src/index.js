3110981f72e384d5f08beec78788e6bafcdc4608
andrusha52 <andrushaes@gmail.com>
2020-01-21 17:36:50 +0200

    upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import "./components/startPage";
 import "./components/startHtmlCss";
 import "./components/default-page/default-page";
-<<<<<<< HEAD
 import "./components/preloader/js";
-=======
 import "./components/startPage/cardItem/card";
->>>>>>> Card_of_item
