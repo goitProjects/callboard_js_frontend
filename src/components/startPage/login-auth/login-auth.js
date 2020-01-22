6c3bed658b656efb81d028fb33f80d239ad74da4
a-sapon <sapon.anya@gmail.com>
2020-01-22 13:26:18 +0200

    fixed errors

--- src/components/startPage/login-auth/login-auth.js
+++ src/components/startPage/login-auth/login-auth.js
@@ -8,32 +8,69 @@ const refs = {
   inputsContainer: document.querySelector('.inputs-container'),
   authBtns: document.querySelector('.auth-btns'),
   registerBlock: document.querySelector('.navigation__registration'),
-  logoutBlock: document.querySelector('.navigation__logout')
+  logoutBlock: document.querySelector('.navigation__logout'),
+  loggedUser: document.querySelector('.logged-user')
 };
 
-services.refs.buttonLogin.addEventListener('click', openModal);
+services.refs.buttonLogin.addEventListener('click', showModal);
+refs.authModalWindow.addEventListener('click', login);
+refs.authModalWindow.addEventListener('click', registerFromModal);
+services.refs.buttonReg.addEventListener('click', registerInit);
 services.refs.logout.addEventListener('click', logoutFromAcc);
 refs.overlay.addEventListener('click', closeModal);
-refs.authModalWindow.addEventListener('click', registerOrLogin);
 
-function openModal() {
+function showModal() {
   refs.overlay.classList.remove('hide');
   refs.overlay.classList.add('show');
 }
 
-function closeModal(e) {
-  if (
-    e.target.classList.contains('close-icon') ||
-    e.target === e.currentTarget
-  ) {
-    refs.overlay.classList.add('hide');
-    refs.overlay.classList.remove('show');
+async function login(e) {
+  if (e.target.classList.contains('btn-login')) {
+    try {
+      const user = {
+        email: e.currentTarget.elements.email.value,
+        password: e.currentTarget.elements.password.value
+      };
+      const dataLogin = await services.postLoginUser(user);
+      services.userData = dataLogin.data.userData;
+      services.token = dataLogin.data.token;
+      services.ads = dataLogin.data.ads;
+      services.favorites = dataLogin.data.favorites;
+      services.isAuth = true;
+      console.log(services);
+
+      refs.registerBlock.style.display = 'none';
+      refs.logoutBlock.style.display = 'block';
+      refs.loggedUser.textContent = services.userData.name;
+
+      refs.overlay.classList.add('hide');
+      refs.overlay.classList.remove('show');
+      PNotify.success({
+        title: 'Success!',
+        text: 'You are logged in.'
+      });
+      localStorage.setItem('token', dataLogin.data.token);
+    } catch (e) {
+      PNotify.error({
+        title: 'Oops!',
+        text: 'Email or password is incorrect.',
+        modules: {
+          Desktop: {
+            desktop: true
+          }
+        }
+      });
+    }
   }
+}
+
+async function registerInit() {
+  showModal();
 }
 
 const user = {};
 
-async function registerOrLogin(e) {
+async function registerFromModal(e) {
   e.preventDefault();
   if (e.target.classList.contains('btn-registration')) {
     const emailInput = e.currentTarget.elements.email;
@@ -89,45 +126,26 @@ async function registerOrLogin(e) {
         });
       }
     }
-  } else if (e.target.classList.contains('btn-login')) {
-    try {
-      const user = {
-        email: e.currentTarget.elements.email.value,
-        password: e.currentTarget.elements.password.value
-      };
-      const dataLogin = await services.postLoginUser(user);
-      services.userData = dataLogin.data.userData;
-      services.token = dataLogin.data.token;
-      services.ads = dataLogin.data.ads;
-      services.favorites = dataLogin.data.favorites;
-      services.isAuth = true;
-      console.log(services);
-
-      refs.overlay.classList.add('hide');
-      refs.overlay.classList.remove('show');
-      PNotify.success({
-        title: 'Success!',
-        text: 'You are logged in.'
-      });
-      localStorage.setItem('token', dataLogin.data.token);
-    } catch (e) {
-      PNotify.error({
-        title: 'Oops!',
-        text: 'Email or password is incorrect.',
-        modules: {
-          Desktop: {
-            desktop: true
-          }
-        }
-      });
-    }
   }
 }
 
 function logoutFromAcc() {
-  services.postLogoutUser({ headers: { Authorization: services.token } });
+  services.postLogoutUser({ headers: { 'Authorization': services.token } });
   localStorage.removeItem('token');
   refs.authModalWindow.reset();
   services.isAuth = false;
+
+  refs.registerBlock.style.display = 'block';
+  refs.logoutBlock.style.display = 'none';
   console.log('LOGGED OUT!!!!!!!!!!');
+}
+
+function closeModal(e) {
+  if (
+    e.target.classList.contains('close-icon') ||
+    e.target === e.currentTarget
+  ) {
+    refs.overlay.classList.add('hide');
+    refs.overlay.classList.remove('show');
+  }
 }
