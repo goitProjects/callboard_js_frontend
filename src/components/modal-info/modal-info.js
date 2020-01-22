0342b68b6a258639e8ffdb9fd8d0597d5bed3c03
Tania <taniameoow@gmail.com>
2020-01-22 22:56:32 +0200

    add changes

--- src/components/modal-info/modal-info.js
+++ src/components/modal-info/modal-info.js
@@ -2,79 +2,86 @@ import services from "../../services.js";
 import template from "./modal-info.hbs";
 import "./modal-info.css";
 
-
-const section=document.querySelector('section');
-// const container = section.querySelector(".mainContainer");
-const wrapper = document.querySelector(".modal-info__wrapper");
+const mainTable = document.querySelector(".modal-info__modal");
 const overlay = document.querySelector(".modal-info__overlay");
-// const div=container.querySelectorAll('div');
-const ul=document.querySelector('.products-collection-computer-list');
-const div=document.querySelector('.products-collection-computer');
+const ul = document.querySelector(".products");
+const section=document.querySelector('section.products')
+const divMain=document.querySelector('.mainTable')
+const body=document.querySelector('body');
+let fav=document.querySelector('.fav');
+
+
+
 ul.addEventListener("click", handleClick);
-overlay.addEventListener("click", handleOverlay);
+section.addEventListener("click", handleOverlay);
 document.addEventListener("keydown", handleKeyPress);
-
-
 
 let svg;
 async function handleClick(e) {
-  // ul.classList.add("show-modal");
+  
+  const li = document.querySelector(".Card_cardItem");
+  overlay.classList.add("show-modal");
+  overlay.style.opacity= "1";
+  overlay.style.display= "block";
+  overlay.style.position= "fixed";
+  overlay.style.display= "flex";
+  overlay.style.justifyContent= "center";
+  overlay.style.alignItems= "center";
+
+  section.style.backgroundColor= "rgba(0, 0, 0, 0.5)";
+  overlay.style.width= "100vw";
+  overlay.style.height= "100vh";
 
   
-  // ul.removeEventListener("click", handleClick);
+  ul.removeEventListener("click", handleClick);
  
-  await services.getUserAds(li.dataset.id).then(res => {
-  console.log(res)
-    show(res.data.goal);
+    await services.getUserAds(li.dataset.id).then(res => {
+      show(res.data.goal);
+
+      let button = document.querySelector(".modal-info__buy-button");
+  
+      let link = document.querySelector(".modal-info__link");
+     
+      button.addEventListener("click", e => {
+        e.target.style.color = "#ff6b08";
+        e.target.style.fontSize = "18px";
+        link.href = "tel:${res.data.goal.phone}";
+        e.target.innerText = res.data.goal.phone;
+        button.style.backgroundColor = "#fff";
+        button.style.border = "2px solid #ff6b08";
+      });
+
+      let closingBtn = document.querySelector(".modal-info__left-mobile-arrow");
+      closingBtn.addEventListener("click", e => {
+        closeModal();
+      });
+
+     
+      let icon = document.querySelector("#modal-info__favorite");
+      icon.addEventListener("click", e => {
+        
+        icon.classList.add('js-fav');
+        fav.style.visibility="visible";
+     
+        fav.style.height = "16px";
+        fav.style.width = "16px";
+
+      });
+     
+      // icon.setAttribute('id', 'modal-info__favorite')
+    });
     
-    let li=document.querySelector('.Card_cardItem');
-    li.classList.add("show-modal");
-    
-    let button = document.querySelector(".modal-info__buy-button");
-    let link = document.querySelector(".modal-info__link");
-
-    button.addEventListener("click", e => {
-      e.target.style.color = "#ff6b08";
-      e.target.style.fontSize = "18px";
-      link.href = "tel:${res.data.goal.phone}";
-      e.target.innerText = res.data.goal.phone;
-      button.style.backgroundColor='#fff';
-      button.style.border= '2px solid #ff6b08'
-
-    });
-
-    let closingBtn = document.querySelector(".modal-info__left-mobile-arrow");
-    closingBtn.addEventListener("click", e => {
-      closeModal();
-    });
-
-    let icon = document.getElementById("modal-info__favorite");
-    icon.addEventListener("click", e => {
-     container.style.backgroundImage="url('../../icons/heart/favorite-heart-button.svg/')";
-     icon.style.width='18px';
-     icon.style.height='18px';
-
-     icon.style.fill='red'
-    //  icon.style.stroke='red'
-      // console.log(heart)
-    });
-      // svg = document.getElementById("favorite");
-      // // icon.replace(icon, 'svg');
-      // icon.parentNode.replaceChild(svg, icon);
-
-      // icon.classList.remove("favorite");
-      // icon.setAttribute('id', 'favorite')
-      // icon = svg;
-      // icon.style.fill="red"
-    });
+  }
+  
   // })
-
-}
 
 
 function closeModal(e) {
-  ul.classList.remove("show-modal");
-  // container.addEventListener("click", handleClick);
+  overlay.classList.remove("show-modal");
+  overlay.style='none';
+  section.style='none';
+  console.log('clicked')
+  // ul.addEventListener("click", handleClick);
 }
 
 function handleKeyPress(e) {
@@ -85,9 +92,12 @@ function handleKeyPress(e) {
 
 function handleOverlay(e) {
   if (e.target !== e.currentTarget) {
-    return;
+ return 
   }
+  
   closeModal();
+ 
+ 
 }
 
 function show(itm) {
@@ -95,7 +105,7 @@ function show(itm) {
 }
 
 function insertItems(item) {
-  div.insertAdjacentHTML("beforeend", item);
+  mainTable.insertAdjacentHTML("beforeend", item);
 }
 
 function createListMarkup(items) {
