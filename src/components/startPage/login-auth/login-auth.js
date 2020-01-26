e59992a18c5d0dc27e0ece9289a73a840c3af5dc
andrusha52 <andrushaes@gmail.com>
2020-01-26 13:05:54 +0200

    merge product and fix bag

--- src/components/startPage/login-auth/login-auth.js
+++ src/components/startPage/login-auth/login-auth.js
@@ -62,7 +62,6 @@ async function login(e) {
 
 function showRegisterModal(e){
   if(e.target.classList=="registration-button" ){
-
     refs.overlayLogin.style.display="none"
   refs.overlayRegister.style.display="flex";}
     if(e.target.classList =='btn-registration'){
@@ -154,8 +153,7 @@ function stayLoggedIn() {
   if(servicesFromLS !== null) {
     refs.registerBlock.style.display = 'none';
     refs.logoutBlock.style.display = 'block';
-    refs.loggedUser.textContent = servicesFromLS.userData.name;
-
+    refs.loggedUser.textContent = "NAME";
     services.userData = servicesFromLS.userData;
     services.token = servicesFromLS.token;
     services.ads = servicesFromLS.ads;
