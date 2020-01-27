d28d33651f63f4e4983ab9dbbefe9ce49a9d210b
YK911 <55898053+YK911@users.noreply.github.com>
2020-01-27 10:52:27 +0200

    commit for pull

--- src/components/showteam/js/index.js
+++ src/components/showteam/js/index.js
@@ -1,24 +1,29 @@
-import "../css/styles.css";
-
-
+import "../css/showTeam.css";
 
 const showTeamBtn = document.querySelector(".info-students");
-const showTeamBox = document.getElementById("show-team__box");
+const showTeamBox = document.querySelector(".team-hide");
 const teamBoxOverlay = document.querySelector(".team__box-overlay");
 const closeTeamBoxBtn = document.querySelector(".team__box-close-btn");
 
-
-
 function handleShowTeam(e) {
   e.preventDefault();
-  showTeamBox.classList.add("is-open");
+  showTeamBox.style.display = "block";
+  // showTeamBox.className = "is-open";
+  // showTeamBox.classList.add("is-open");
 }
 
 function handleCloseBox(e) {
-  if (e.target === teamBoxOverlay || e.target === closeTeamBoxBtn) {
-    showTeamBox.classList.remove("is-open");
-  }
+  console.log(e);
+  // if (e.target === teamBoxOverlay || e.target === closeTeamBoxBtn) {
+  // showTeamBox.classList.remove("is-close");
+  showTeamBox.style.display = "none";
+  // }
 }
 
-// showTeamBtn.addEventListener("click", handleShowTeam);
-// showTeamBox.addEventListener("click", handleCloseBox);
+showTeamBtn.addEventListener("click", handleShowTeam);
+closeTeamBoxBtn.addEventListener("click", handleCloseBox);
+
+
+console.log(window.addEventListener('click', (e) => {
+  console.log(e.target)
+}))
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import "./components/startHtmlCss";
-import "./components/default-page/default-page";
-import "./components/preloader/js";
+// import "./components/default-page/default-page";
+// import "./components/preloader/js";
 import "./components/showteam/js";
-import "./components/startPage/cardItem/card";
+// import "./components/startPage/cardItem/card";
 import "../src/components/startPage/userAccount/userAccount.js";
-import '../src/components/startPage/login-auth/login-auth';
-import '../src/components/startPage/login-auth/login-auth.css';
-import "../src/components/startPage/userAccount/userAccount"
+import "../src/components/startPage/login-auth/login-auth";
+import "../src/components/startPage/login-auth/login-auth.css";
+import "../src/components/startPage/userAccount/userAccount";
 import "./components/startPage/photo-conversion/photo-conversion";
 import "./components/serachBar/index.js";
 import "./components/startPage/login-auth/login-auth";
