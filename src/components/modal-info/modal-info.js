cfc4c0c6dc62d0ef7fef93aa56f416d6ae7de433
andrusha52 <andrushaes@gmail.com>
2020-01-28 09:05:47 +0200

    fix login logout reg / add pnotify mob

--- src/components/modal-info/modal-info.js
+++ src/components/modal-info/modal-info.js
@@ -2,6 +2,7 @@ import services from "../../services.js";
 import template from "./modal-info.hbs";
 import "./modal-info.css";
 import PNotify_1 from "pnotify/dist/es/PNotify";
+import PNotifyMobile from 'pnotify/dist/es/PNotifyMobile';
 
 
 const mainTable = document.querySelector(".modal-info__modal");
@@ -12,10 +13,10 @@ overlay.addEventListener("click", handleOverlay);
 document.addEventListener("keydown", handleKeyPress);
 
 let svg;
-localStorage.setItem(
-  "token",
-  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlMjU4NjY3MDZjODI3MzdmYzI3ZjY0ZSIsImlhdCI6MTU3OTc5NDU3NX0.UFCcUUw7UEESSQVnLAc9io5hsu1tQXFA6dY0peYafD8"
-);
+// localStorage.setItem(
+//   "token",
+//   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlMjU4NjY3MDZjODI3MzdmYzI3ZjY0ZSIsImlhdCI6MTU3OTc5NDU3NX0.UFCcUUw7UEESSQVnLAc9io5hsu1tQXFA6dY0peYafD8"
+// );
 const tmp = localStorage.getItem("token");
 
 // FETCHING DATA AND RENDERING
